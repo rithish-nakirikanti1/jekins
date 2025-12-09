b431391bd5ef4becf719ea8e71d8c0ba4fc7f920
rithish-nakirikanti1 <rithishnakirikanti720@gmail.com>
2025-12-10 01:24:53 +0530

    Update greeting message in HelloJenkins

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,5 +1,5 @@
 public class HelloJenkins {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Freestyle Job!");
+        System.out.println("Hello from Jenkins Freestyle Job!rithish");
     }
 }
